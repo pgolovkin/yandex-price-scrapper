e6fca2585bdf6799d23cfe7c007de6aa5c315803
pgolovkin <golovkin@soramitsu.co.jp>
2022-04-03 08:39:46 +0300

    Change the approach for getting the env variables

--- scrapper.py
+++ scrapper.py
@@ -22,10 +22,10 @@ GOODS = [
 
 USD_CODE = "840"
 GOOGLE_SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-GOOGLE_DOC_ID = os.environ.get('GOOGLE_DOC_ID')
-GOOGLE_TOKEN = json.loads(os.environ.get('GOOGLE_TOKEN'))
+GOOGLE_DOC_ID = os.getenv('GOOGLE_DOC_ID')
+GOOGLE_TOKEN = json.loads(os.getenv('GOOGLE_TOKEN'))
 CBR_RATE_URL = "https://www.cbr.ru/currency_base/daily/"
-proxies = json.loads(os.environ.get('PROXY_LIST'))
+proxies = json.loads(os.getenv('PROXY_LIST'))
 
 
 def update_sheet(current_date, cbr_rate_value, price_values):
